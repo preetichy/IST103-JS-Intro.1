767802b206e4d7ba547dc5f5da02d89f4f1af595
preetichy <166168354+preetichy@users.noreply.github.com>
2024-04-11 14:06:28 -0700

    added doll

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let toDoList = [
   "Watch Netflix",
   "Night Walk",
 ];
+let Doll = {
+  type: "Duck",
+  color: "Yellow",
+  brand: "Peach",
+  hasAccessories: true,
+  accessories: ["Dress", "Cap", "Tail"],
+  play: function () {
+    console.log("Playing with the " + this.type);
+  },
+};
