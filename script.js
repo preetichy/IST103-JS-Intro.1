5ff8092fa04fc93a7452a0489ac9509dcf792336
preetichy <166168354+preetichy@users.noreply.github.com>
2024-04-11 14:24:40 -0700

    Added constants and console.log statements

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ let address = {
 }; // object
 let job = null; // null
 let spouse; // undefined
+
+const SPEED_OF_LIGHT = 299792458; // meters per second
+
+console.log("Person's name is " + name);
+console.log("Person's age is " + age);
+console.log("Is the person a student? " + (isStudent ? "Yes" : "No"));
+console.log("Person's height is " + height + " cm");
+console.log("Person's hobbies include: " + hobbies.join(", "));
+console.log(
+  "Person's address is: " +
+    address.street +
+    ", " +
+    address.city +
+    ", " +
+    address.zip
+);
+console.log("Person's job status is " + job);
+console.log("Person's spouse status is " + spouse);
+console.log("The speed of light is " + SPEED_OF_LIGHT + " meters per second");
